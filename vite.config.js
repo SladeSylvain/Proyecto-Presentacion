d1a4d09cb29ff504cadae32307ba13fcac60cfe5
id:001 <alexcarvajal@outlook.fr>
2024-06-28 19:22:56 -0400

    waffles

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/Proyecto-Presentacion/', // Reemplaza 'Proyecto-Presentacion' con el nombre de tu repositorio en GitHub
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: '/Proyecto-Presentacion/', // Reemplaza 'Proyecto-Presentacion' con el nombre de tu repositorio en GitHub
+  }
 });
